6d974b48a5fe95350b6556eeb034bf34969903f9
yananmao <yananmao@hengtiansoft.com>
2019-02-27 17:37:24 +0800

    2019-02-27

--- 01-matrix.py
+++ 01-matrix.py
@@ -1,29 +1,12 @@
 import collections;
 class Solution:
   def updateMatrix(self, matrix):
-      """
-      :type matrix: List[List[int]]
-      :rtype: List[List[int]]
-      """
-      self.nRow, self.nCol = len(matrix), len(matrix[0])
-      self.ans = [['inf'] * self.nCol for i in range(self.nRow)]
-      queue = collections.deque()
-      
-      for r in range(self.nRow):
-          for c in range(self.nCol):
-              if matrix[r][c] == 0:
-                  self.ans[r][c] = 0
-                  queue.append((r,c))
-      self.bfs(queue)
-      return self.ans
-  
-  def bfs(self, queue):
-      while queue:
-          (r, c) = queue.popleft()
-          for (d_r, d_c) in [(1,0), (-1,0), (0,1), (0,-1)]:
-              n_r, n_c = r+d_r, c+d_c
-              if 0 <= n_r < self.nRow and 0 <= n_c < self.nCol and self.ans[r][c]+1 < self.ans[n_r][n_c]:
-                  self.ans[n_r][n_c] = self.ans[r][c]+1
-                  queue.append((n_r, n_c))
+    # 定义全局变量的两种方式,另一种是使用 global
+    self.cols = len(matrix);
+    self.lines = len(matrix[0]);
+    # 生成一个跟matrix大小相同的矩阵来存储返回值
+    res = [[-1]*self.lines for i in range(self.cols)];
+    for 
+
 print(Solution().updateMatrix([[0,1,1]]))
 
