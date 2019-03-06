bd17bf12eb81bf8689136bc74ee3f557c4696317
yananmao <yananmao@hengtiansoft.com>
2019-03-06 17:32:35 +0800

    2019-03-06

---
+++ answers/01-matrix/01-matrix.js
@@ -0,0 +1,12 @@
+var maxProfit = function(prices) {
+    maxPro = 0;
+    minPri = prices[0];
+    for(var i=0,len=prices.length;i<len;i++){
+      if(prices[i]<minPri){
+        minPri = prices[i];
+      }else{
+        maxPro = Math.max(maxPro,prices[i]-minPri)
+      }
+    }
+    return maxPro
+};
